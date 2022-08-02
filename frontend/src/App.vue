43fa618de485aa2cd819b1d2092c0c3256fbc1c3
Aarón Carrizal <aaronk4572@gmail.com>
2022-08-02 18:29:48 -0500

    added basic components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>Hola</h1>
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
---
+++ frontend/src/components/SongForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>SongForm</h1>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
