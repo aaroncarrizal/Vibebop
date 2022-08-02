fd62fbd0b02bbec75c1e255d649f51246e7754b9
Aarón Carrizal <aaronk4572@gmail.com>
2022-08-01 20:03:49 -0500

    added db connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,5 @@
-import app from "./app";
-app.listen(3000);
-console.log('Server is running on port 3000');
+import app from "./app"
+import {startConnection} from './database'
+startConnection();
+app.listen(3000)
+console.log('Server is running on port 3000')
