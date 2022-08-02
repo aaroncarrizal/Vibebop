4cce179802b3c405e63b7cd9fbc35e339784044a
Aarón Carrizal <aaronk4572@gmail.com>
2022-08-01 20:31:57 -0500

    changed response body and removed ;

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -5,27 +5,27 @@ const router = Router()
 
 router.get('/', (req, res) => {
     res.send("REST API is working!")
-});
+})
 
 // Songs routes
 router.get('/songs', async (req, res) => {
     const songs = await Song.find()
     res.send(songs)
-});
+})
 
 router.post('/songs', async (req, res) => {
     const {title, lyrics} = req.body
     const song = new Song({title, lyrics})
     await song.save()
-    res.send("Song created with the values" + song)
-});
+    res.json(song)
+})
 
 router.patch('/songs', async (req, res) => {
     const songs = await Song.find()
     res.send(songs)
-});
+})
 
 router.get('/songs/:id', (req, res) => {
     res.send("Displaying one song")
-});
+})
 export default router
