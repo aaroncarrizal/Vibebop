f5bc18e6ce444a0a50e2c90b820000339cac2c55
Aarón Carrizal <aaronk4572@gmail.com>
2022-08-01 20:07:49 -0500

    removed ;

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 
-import routes from './routes';
+import routes from './routes'
 
-const app = express();
-app.use(morgan('dev'));
-app.use(express.json());
-app.use("/api", routes);
-export default app;
+const app = express()
+app.use(morgan('dev'))
+app.use(express.json())
+app.use("/api", routes)
+export default app
