0035669964f13d410c41469bc26edf9652e2f762
Aarón Carrizal <aaronk4572@gmail.com>
2022-08-01 20:22:13 -0500

    added songs post & get routes

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,12 +1,30 @@
 import { Router } from "express"
+import Song from '../models/Song'
 const router = Router()
+
+
 router.get('/', (req, res) => {
     res.send("REST API is working!")
 });
 
-router.get('/songs', (req, res) => {
-    res.send("Displaying all songs")
+// Songs routes
+router.get('/songs', async (req, res) => {
+    const songs = await Song.find()
+    res.send(songs)
 });
+
+router.post('/songs', async (req, res) => {
+    const {title, lyrics} = req.body
+    const song = new Song({title, lyrics})
+    await song.save()
+    res.send("Song created with the values" + song)
+});
+
+router.patch('/songs', async (req, res) => {
+    const songs = await Song.find()
+    res.send(songs)
+});
+
 router.get('/songs/:id', (req, res) => {
     res.send("Displaying one song")
 });
