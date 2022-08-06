6816f777213a53101ea5c4c820acec4c23ed3436
Aarón Carrizal <aaronk4572@gmail.com>
2022-08-06 17:50:40 -0500

    added SongForm component functionality

--- frontend/src/components/SongForm.vue
+++ frontend/src/components/SongForm.vue
@@ -1,11 +1,33 @@
 <template>
-  <h1>SongForm</h1>
+    <form @submit.prevent="saveSong()">
+        <input type="text" placeholder="Song title" v-model="song.title">
+        <textarea cols="30" rows="10" placeholder="Song lyrics" v-model="song.lyrics"></textarea>
+        <button>Save</button>
+    </form>
 </template>
 
 <script lang="ts">
-export default {
+import { Song } from '@/interfaces/Song'
+import { defineComponent } from 'vue'
+import { createSong } from '@/services/SongService'
 
-}
+export default defineComponent({
+    data(){
+        return{
+            song: {} as Song
+        }
+    },
+    methods:{
+        async saveSong(){
+            try{
+                const res = await createSong(this.song)
+                console.log(res)
+            }catch(err){
+                console.log(err)
+            }
+        }
+    }
+})
 </script>
 
 <style>
