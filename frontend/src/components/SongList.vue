4e4958e4d2d2b42034f2cc8d16cef567a369c630
Aarón Carrizal <aaronk4572@gmail.com>
2022-08-06 18:14:31 -0500

    added songlist component functionality

--- frontend/src/components/SongList.vue
+++ frontend/src/components/SongList.vue
@@ -1,13 +1,37 @@
 <template>
-  <h1>SongList</h1>
+    <h1>SongList</h1>
+    <template v-for="(song, index) in songs" :key="index">
+        <div>{{ song.title }}</div>
+        <div>{{ song.lyrics }}</div>
+    </template>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue'
+import { getSongs } from '@/services/SongService'
+import { Song } from '@/interfaces/Song'
 
-}
+export default defineComponent({
+    data(){
+        return {
+            songs: [] as Song[]
+        }
+    },
+    mounted(){
+        this.loadSongs()
+    },
+    methods:{
+        async loadSongs(){
+            try {
+                const res = await getSongs();
+                this.songs = res.data
+            } catch (err) {
+                console.log(err)
+            }
+        },
+    },
+})
 </script>
 
 <style>
-
 </style>
--- frontend/src/services/SongService.ts
+++ frontend/src/services/SongService.ts
@@ -4,3 +4,7 @@ import { Song } from '@/interfaces/Song'
 export const createSong = async (song: Song) => {
     return await axios.post('/songs', song)
 }
+
+export const getSongs = async () => {
+    return await axios.get('/songs')
+}
