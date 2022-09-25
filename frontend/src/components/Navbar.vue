8946585968af4137cbec3975cbd11534f3f26f00
Aarón Carrizal <aaronk4572@gmail.com>
2022-09-24 20:01:41 -0500

    fixed nabar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg bg-light mb-3">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">Navbar</a>
             <button 
